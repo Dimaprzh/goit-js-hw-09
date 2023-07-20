dd6b791fbbbd37a4d2840fce6bd899ec9125b8d7
Dimaprzh <dimaprzh@ukr.net>
2023-07-20 21:38:34 +0300

    Update 02 - video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,15 +12,4 @@ import throttle from 'lodash.throttle';
 
  const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
- player.setCurrentTime(currentTime).then(function (seconds) {
-     // seconds = the actual time that the player seeked to
- }).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-           break;
-       default:
-            // some other error occurred
-            break;
-    }
- });
+ player.setCurrentTime(currentTime || 0)
